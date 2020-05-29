49d7d0d96caaa4130727d0137a3f52acd7949c63
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-29 18:59:42 -0300

    Relatorio

--- src/Program.java
+++ src/Program.java
@@ -5,43 +5,78 @@ public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in); 
-		
+		Scanner sc = new Scanner(System.in);
+
 		char opcao;
-		String nome; 
-		double altura, peso;
-		
+		String nome, maisAlto = "qualquer";
+		int qntAtletas = 0;
+		double altura, maiorAltura = 0, peso, pesoMedio;
+		double somaPesos = 0; 
+		double homens = 0; 
+		double mulheres = 0; 
+		double porcentagemHomens;
+		double somaAltura = 0, alturaMedia; 
+
 		do {
-			
+
 			System.out.print("Nome: ");
-			nome = sc.nextLine(); 
-			
+			nome = sc.nextLine();
+
 			System.out.print("Sexo: ");
-			char sexo = sc.next().charAt(0); 
-			while(sexo != 'F' && sexo != 'M') {
-				System.out.print("Resposta inválida, tente novamente: " );
-				sexo = sc.next().charAt(0); 
+			char sexo = sc.next().charAt(0);
+			while (sexo != 'F' && sexo != 'M') {
+				System.out.print("Resposta inválida, tente novamente: ");
+				sexo = sc.next().charAt(0);
 			}
-			
+
 			System.out.print("Altura: ");
-			altura = sc.nextDouble(); 
-			while(altura < 0) {
+			altura = sc.nextDouble();
+			while (altura < 0) {
 				System.out.print("A altura não pode ser negativa. Tente novamente: ");
-				altura = sc.nextDouble();  
+				altura = sc.nextDouble();
 			}
-			
+
 			System.out.print("Peso: ");
 			peso = sc.nextDouble();
-			while(peso < 0) {
+			while (peso < 0) {
 				System.out.print("O peso não pode ser negativo. Tente novamente: ");
-				 peso = sc.nextDouble();  
+				peso = sc.nextDouble();
 			}
-			
-			System.out.print("Digitar outro atleta (S/N)?");
+
+			System.out.print("Digitar outro atleta (S/N)? ");
 			opcao = sc.next().charAt(0);
 			
+			if(altura > maiorAltura) { //definir atleta mais alto
+				maiorAltura = altura;
+				maisAlto = nome; 
+			}
+			if(sexo == 'M') {
+				homens++; 
+			}
+			if(sexo == 'F') {
+				mulheres++;
+				somaAltura += altura; 
+			}
+			somaPesos += peso;
+			qntAtletas++; 
+			
 			sc.nextLine();
-		}while(opcao != 'N');
+		} while (opcao != 'N');
+		
+		pesoMedio = somaPesos / qntAtletas; // calculo do peso
+		
+		porcentagemHomens = (homens/qntAtletas) * 100; //porcentagem de homens
+		
+		alturaMedia = somaAltura / mulheres; //altura media das mulheres
+		
+		System.out.println("");
+		System.out.println("RELATORIO: ");
+		System.out.printf("Peso medio dos atletas: %.2f\n", pesoMedio);
+		System.out.println("Atleta mais alto: " + maisAlto);
+		System.out.printf("Procentagem de homens: %.1f%%\n", porcentagemHomens);
+		System.out.printf("Altura media das mulheres: %.2f\n", alturaMedia);
+		System.out.println();
+		System.out.println("FIM DO PROGRAMA!");
 
 	}
 
